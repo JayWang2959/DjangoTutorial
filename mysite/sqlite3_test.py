d8e8b4dd6343d7ff6e58b57bd631f9a7d738ddf4
JayWang2959 <2643267669@qq.com>
2021-04-15 15:20:26 +0800

    第4-5部分

--- mysite/sqlite3_test.py
+++ mysite/sqlite3_test.py
@@ -1,7 +1,11 @@
 import sqlite3
+from polls.models import Choice, Question
 
 
-conn = sqlite3.connect('db.sqlite3')
-cur = conn.cursor()
-cur.execute("select name from sqlite_master where type='table' order by name")
-print(cur.fetchall())
+# conn = sqlite3.connect('db.sqlite3')
+# cur = conn.cursor()
+# cur.execute("select name from sqlite_master where type='table' order by name")
+# print(cur.fetchall())
+
+
+print(Question.objects.all())
